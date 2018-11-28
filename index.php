f6f3fad481b7aae77a7c7267136f3ea621abf30c
Chaitanya Chawla <chaitanyachawla97@gmail.com>
2018-11-28 12:52:01 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,328 @@
+<?php include ( "inc/connect.inc.php" ); ?>
+<?php 
+ob_start();
+session_start();
+if (!isset($_SESSION['user_login'])) {
+	$user = "";
+}
+else {
+	$user = $_SESSION['user_login'];
+	$result = mysql_query("SELECT * FROM user WHERE id='$user'");
+		$get_user_email = mysql_fetch_assoc($result);
+			$uname_db = $get_user_email['firstName'];
+}
+?>
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Welcome to plants and pots</title>
+		<link rel="stylesheet" type="text/css" href="css/style.css">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+		<script src="/js/homeslideshow.js"></script>
+                <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+                <style>
+* {
+    box-sizing: border-box;
+}
+
+body {
+    margin: 0;
+}
+
+.navbar {
+    overflow: hidden;
+    background-color: #333;
+    font-family: Arial, Helvetica, sans-serif;
+}
+
+.navbar a {
+    float: left;
+    font-size: 16px;
+    color: white;
+    text-align: center;
+    padding: 14px 16px;
+    text-decoration: none;
+}
+
+.dropdown {
+    float: left;
+    overflow: hidden;
+}
+
+.dropdown .dropbtn {
+    font-size: 16px;    
+    border: none;
+    outline: none;
+    color: white;
+    padding: 14px 16px;
+    background-color: inherit;
+    font: inherit;
+    margin: 0;
+}
+
+.navbar a:hover, .dropdown:hover .dropbtn {
+    background-color: red;
+}
+
+.dropdown-content {
+    display: none;
+    position: absolute;
+    background-color: #f9f9f9;
+    width: 100%;
+    left: 0;
+    box-shadow: 0px 8px 16px 0px rgba(0,0,0,0.2);
+    z-index: 1;
+}
+
+.dropdown-content .header {
+    background: red;
+    padding: 16px;
+    color: white;
+}
+
+.dropdown:hover .dropdown-content {
+    display: block;
+}
+
+/* Create three equal columns that floats next to each other */
+.column {
+    float: left;
+    width: 33.33%;
+    padding: 10px;
+    background-color: #ccc;
+    height: 250px;
+}
+
+.column a {
+    float: none;
+    color: black;
+    padding: 16px;
+    text-decoration: none;
+    display: block;
+    text-align: left;
+}
+
+.column a:hover {
+    background-color: #ddd;
+}
+
+/* Clear floats after the columns */
+.row:after {
+    content: "";
+    display: table;
+    clear: both;
+}
+
+/* Responsive layout - makes the three columns stack on top of each other instead of next to each other */
+@media screen and (max-width: 600px) {
+    .column {
+        width: 100%;
+        height: auto;
+    }
+}
+</style>
+	</head>
+	<body style="min-width: 980px;">
+            
+		<div class="homepageheader" style="position: relative;">
+     
+			<div class="signinButton loginButton">
+				<div class="uiloginbutton signinButton loginButton" style="margin-right: 40px;">
+                                              
+					<?php 
+						if ($user!="") {
+							echo '<a  href="logout.php">LOG OUT</a>';
+						}
+						else {
+							echo '<a  href="signin.php">SIGN IN</a>';
+						}
+					 ?>
+					
+				</div>
+				<div class="uiloginbutton signinButton loginButton" style="">
+					<?php 
+						if ($user!="") {
+							echo '<a  href="profile.php?uid='.$user.'">Hi '.$uname_db.'</a>';
+						}
+						else {
+							echo '<a  href="login.php">LOG IN</a>';
+						}
+					 ?>
+				</div>
+			</div>
+			<div style="float: left; margin: 5px 0px 0px 23px;">
+				                <div class="navbar">
+  
+  <div class="dropdown">
+    <button class="dropbtn">Pots and Planters
+      <i class="fa fa-caret-down"></i>
+    </button>
+    <div class="dropdown-content">
+      <div class="header">
+        <h2>Pots and Planters</h2>
+      </div>   
+      <div class="row">
+        <div class="column">
+          <h3>Category 1</h3>
+          <a href="women/ornament.php">Planters</a>
+          <a href=women/saree.php">Glass pots</a>
+          <a href="#">Link 3</a>
+        </div>
+        <div class="column">
+          <h3>Category 2</h3>
+          <a href="#">Link 1</a>
+          <a href="#">Link 2</a>
+          <a href="#">Link 3</a>
+        </div>
+        <div class="column">
+          <h3>Category 3</h3>
+          <a href="#">Link 1</a>
+          <a href="#">Link 2</a>
+          <a href="#">Link 3</a>
+        </div>
+      </div>
+    </div>
+  </div> 
+                                                    <div class="dropdown">
+    <button class="dropbtn">Dropdown 
+      <i class="fa fa-caret-down"></i>
+    </button>
+    <div class="dropdown-content">
+      <div class="header">
+        <h2>Mega Menu</h2>
+      </div>   
+      <div class="row">
+        <div class="column">
+          <h3>Category 1</h3>
+          <a href="#">Link 1</a>
+          <a href="#">Link 2</a>
+          <a href="#">Link 3</a>
+        </div>
+        <div class="column">
+          <h3>Category 2</h3>
+          <a href="#">Link 1</a>
+          <a href="#">Link 2</a>
+          <a href="#">Link 3</a>
+        </div>
+        <div class="column">
+          <h3>Category 3</h3>
+          <a href="#">Link 1</a>
+          <a href="#">Link 2</a>
+          <a href="#">Link 3</a>
+        </div>
+      </div>
+    </div>
+  </div> 
+                                                    <div class="dropdown">
+    <button class="dropbtn">Dropdown 
+      <i class="fa fa-caret-down"></i>
+    </button>
+    <div class="dropdown-content">
+      <div class="header">
+        <h2>Mega Menu</h2>
+      </div>   
+      <div class="row">
+        <div class="column">
+          <h3>Category 1</h3>
+          <a href="#">Link 1</a>
+          <a href="#">Link 2</a>
+          <a href="#">Link 3</a>
+        </div>
+        <div class="column">
+          <h3>Category 2</h3>
+          <a href="#">Link 1</a>
+          <a href="#">Link 2</a>
+          <a href="#">Link 3</a>
+        </div>
+        <div class="column">
+          <h3>Category 3</h3>
+          <a href="#">Link 1</a>
+          <a href="#">Link 2</a>
+          <a href="#">Link 3</a>
+        </div>
+      </div>
+    </div>
+  </div> 
+</div>
+			</div>
+			<div class="">
+				
+				</div>
+			</div>
+		</div>
+		<div class="home-welcome">
+			<div class="home-welcome-text" style="background-image: url(image/116.png); height: 380px; ">
+				<h1 style="margin: 0px;">Welcome To Plants and Pots</h1>
+				<h2>Largest Plant Online Shopping In India</h2>
+			</div>
+		</div>
+          
+		<div class="home-prodlist">
+			<div>
+				<h3 style="text-align: center;">Products Category</h3>
+			</div>
+         
+			<div style="padding: 20px 30px; width: 85%; margin: 0 auto;">
+				<ul style="float: left;">
+					<li style="float: left; padding: 25px;">
+						<div class="home-prodlist-img"><a href="women/saree.php">
+                                                        <img src="./image/1.jpg" class="home-prodlist-imgi">
+							</a>
+						</div>
+					</li>
+				</ul>
+				<ul style="float: left;">
+					<li style="float: left; padding: 25px;">
+						<div class="home-prodlist-img"><a href="women/perfume.php">
+                                                        <img src="./image/2.jpg" class="home-prodlist-imgi">
+							</a>
+						</div>
+					</li>
+				</ul>
+				<ul style="float: left;">
+					<li style="float: left; padding: 25px;">
+						<div class="home-prodlist-img"><a href="women/hijab.php">
+                                                        <img src="./image/3.jpg" class="home-prodlist-imgi"></a>
+						</div>
+					</li>
+				</ul>
+				<ul style="float: left;">
+					<li style="float: left; padding: 25px;">
+						<div class="home-prodlist-img"><a href="women/toilatry.php">
+							<img src="./image/11.png" class="home-prodlist-imgi"></a>
+						</div>
+					</li>
+				</ul>
+				<ul style="float: left;">
+					<li style="float: left; padding: 25px;">
+						<div class="home-prodlist-img"><a href="women/footwear.php">
+							<img src="./image/11.png" class="home-prodlist-imgi"></a>
+						</div>
+					</li>
+				</ul>
+				<ul style="float: left;">
+					<li style="float: left; padding: 25px;">
+						<div class="home-prodlist-img"><a href="women/tshirt.php">
+							<img src="./image/11.png" class="home-prodlist-imgi"></a>
+						</div>
+					</li>
+				</ul>
+				<ul style="float: left;">
+					<li style="float: left; padding: 25px;">
+						<div class="home-prodlist-img"><a href="women/watch.php">
+							<img src="./image/11.png" class="home-prodlist-imgi"></a>
+						</div>
+					</li>
+				</ul>
+				<ul style="float: left;">
+					<li style="float: left; padding: 25px;">
+						<div class="home-prodlist-img"><a href="women/ornament.php">
+							<img src="./image/11.png" class="home-prodlist-imgi"></a>
+						</div>
+					</li>
+				</ul>
+			</div>			
+		</div>
+	</body>
+</html>
